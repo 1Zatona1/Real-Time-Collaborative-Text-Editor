7a32e6a6cf99665473845df0513a3cf295976d70
1Zatona1 <omar.ashrafofficial1@gmail.com>
2025-05-05 00:44:05 +0300

    Fixed merge conflicts

---
+++ src/main/java/com/example/apt_project/HelloController.java
@@ -0,0 +1,115 @@
+package com.example.apt_project;
+
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.concurrent.ExecutionException;
+
+public class HelloController {
+
+    @FXML
+    public ImageView newDocImg;
+    public ImageView browseImg;
+    public ImageView joinImg;
+    public Button browseBtn;
+    public Button joinBtn;
+    public TextField sessionField;
+    public Button newDocBtn;
+    public Label sessionLabel;
+    public Label titleLabel;
+    public File userFile;
+    public String userFileContent;
+
+    @FXML
+    public void handleNewDocBtn() throws IOException {
+
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("NewDocument.fxml"));
+        Parent root = loader.load();
+
+        Stage mainStage = (Stage) newDocBtn.getScene().getWindow();
+        mainStage.close();
+
+        Stage newDocStage = new Stage();
+        Scene newDocScene = new Scene(root, 800, 600);
+        newDocScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        newDocStage.setTitle("Real-Time Collaborative Text Editor");
+        newDocStage.setScene(newDocScene);
+        newDocStage.show();
+        newDocStage.setMaximized(true);
+    }
+
+    public void handleBrowse() throws IOException {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select a text file (.txt)");
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Text files (*.txt)", "*.txt");
+        fileChooser.getExtensionFilters().add(extFilter);
+
+        Stage browseDocStage = new Stage();
+        userFile = fileChooser.showOpenDialog(browseDocStage);
+        if (userFile != null) {
+            try {
+                userFileContent = Files.readString(userFile.toPath());
+            } catch (IOException e) {
+                System.out.println("Error Reading User File: " + e);
+            }
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("BrowseDocument.fxml"));
+            Parent root = loader.load();
+
+            Stage mainStage = (Stage) browseBtn.getScene().getWindow();
+            mainStage.close();
+
+            BrowseDocument controller = loader.getController();
+            controller.setFileContent(userFileContent);
+            Scene browseDocScene = new Scene(root);
+            browseDocScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+            browseDocStage.setTitle("Real-Time Collaborative Text Editor");
+            browseDocStage.setScene(browseDocScene);
+            browseDocStage.setMaximized(true);
+            browseDocStage.show();
+        }
+    }
+
+    @FXML
+    public void handleJoinBtn() throws IOException {
+
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("NewDocument.fxml"));
+        Parent root = loader.load();
+
+
+        Stage mainStage = (Stage) joinBtn.getScene().getWindow();
+        mainStage.close();
+
+        Stage newDocStage = new Stage();
+        Scene newDocScene = new Scene(root, 800, 600);
+        newDocScene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+        newDocStage.setTitle("Real-Time Collaborative Text Editor");
+        newDocStage.setScene(newDocScene);
+        newDocStage.show();
+        newDocStage.setMaximized(true);
+
+    }
+
+    private void showAlert(String title, String message)
+    {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+}
--- src/main/java/com/example/apt_project/WebSocketHandler.java
+++ src/main/java/com/example/apt_project/WebSocketHandler.java
@@ -1,16 +1,17 @@
 package com.example.apt_project;
 
+import org.springframework.messaging.converter.CompositeMessageConverter;
 import org.springframework.messaging.converter.MappingJackson2MessageConverter;
-import org.springframework.messaging.simp.stomp.StompCommand;
-import org.springframework.messaging.simp.stomp.StompHeaders;
-import org.springframework.messaging.simp.stomp.StompSession;
-import org.springframework.messaging.simp.stomp.StompSessionHandlerAdapter;
+import org.springframework.messaging.converter.MessageConverter;
+import org.springframework.messaging.converter.StringMessageConverter;
+import org.springframework.messaging.simp.stomp.*;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 import org.springframework.web.socket.messaging.WebSocketStompClient;
 import org.springframework.web.socket.sockjs.client.SockJsClient;
 import org.springframework.web.socket.sockjs.client.Transport;
 import org.springframework.web.socket.sockjs.client.WebSocketTransport;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -19,24 +20,25 @@ public class WebSocketHandler {
 
     public void connectToWebSocket() {
         try {
+            // WebSocket transport
             List<Transport> transports = Collections.singletonList(new WebSocketTransport(new StandardWebSocketClient()));
             SockJsClient sockJsClient = new SockJsClient(transports);
 
+            // WebSocket Stomp client
             WebSocketStompClient stompClient = new WebSocketStompClient(sockJsClient);
-
-            MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
-            converter.getObjectMapper().findAndRegisterModules();
-            stompClient.setMessageConverter(converter);
-
-            String url = "ws://localhost:8080/ws";
-            MyStompSessionHandler sessionHandler = new MyStompSessionHandler();
+            List<MessageConverter> converters = new ArrayList<>();
+            converters.add(new StringMessageConverter());
+            stompClient.setMessageConverter(new CompositeMessageConverter(converters));
+            // Connect to WebSocket server
+            String url = "ws://localhost:8080/ws"; // WebSocket endpoint
+            StompSessionHandler sessionHandler = new MyStompSessionHandler();
             stompSession = stompClient.connectAsync(url, sessionHandler).get();
 
-            System.out.println("Connected to WebSocket server");
         } catch (Exception e) {
-            System.out.println("Websocket connection failed: " + e.getMessage());
+            System.out.println("WebSocket connection failed: " + e.getMessage());
         }
     }
+
 
 
 }
