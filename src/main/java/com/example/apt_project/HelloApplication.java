0d436169747f419540a9c248aca4735d58f4755f
1Zatona1 <omar.ashrafofficial1@gmail.com>
2025-05-07 05:44:38 +0300

    trying to fix some errors

--- src/main/java/com/example/apt_project/HelloApplication.java
+++ src/main/java/com/example/apt_project/HelloApplication.java
@@ -12,6 +12,7 @@ public class HelloApplication extends Application
     @Override
     public void start(Stage stage) throws IOException
     {
+        //
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
